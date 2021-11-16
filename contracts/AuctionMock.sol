97e18c93486052e3073d0e3b1e424996c1ba90c3
Vinicius Macelai <vinicius.macelai@gmail.com>
2021-11-16 18:47:22 -0300

    feat: add Auction contract and start testing to create listing and place bids

---
+++ contracts/AuctionMock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Auction.sol";
+
+contract SoundchainAuctionMock is SoundchainAuction {
+    uint256 public nowOverride;
+
+    constructor(address payable _platformFeeRecipient, uint16 _platformFee) SoundchainAuction(_platformFeeRecipient, _platformFee) {}
+
+    function setNowOverride(uint256 _now) external {
+        nowOverride = _now;
+    }
+
+    function _getNow() internal override view returns (uint256) {
+        return nowOverride;
+    }
+}
---
+++ test/Auction.test.ts
@@ -0,0 +1,243 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { ethers } from "hardhat";
+import {
+  Soundchain721,
+  Soundchain721__factory,
+  SoundchainAuctionMock__factory,
+} from "../typechain";
+
+describe("Auction and Soundchain Token", () => {
+  const firstTokenId = "0";
+  const secondTokenId = "1";
+  const platformFee = "25"; // marketplace platform fee: 2.5%
+  const tokenUri = "ipfs";
+
+  let owner: SignerWithAddress,
+    minter: SignerWithAddress,
+    buyer: SignerWithAddress,
+    buyer2: SignerWithAddress,
+    nft: Soundchain721,
+    feeAddress: SignerWithAddress,
+    auction: Contract;
+
+  beforeEach(async () => {
+    [owner, minter, buyer, feeAddress, buyer2] = await ethers.getSigners();
+
+    const SoundchainCollectible: Soundchain721__factory =
+      await ethers.getContractFactory("Soundchain721");
+    nft = await SoundchainCollectible.deploy();
+
+    const AuctionFactory: SoundchainAuctionMock__factory =
+      await ethers.getContractFactory("SoundchainAuctionMock");
+
+    auction = await AuctionFactory.deploy(feeAddress.address, platformFee);
+
+    await nft.safeMint(minter.address, tokenUri);
+    await nft.safeMint(owner.address, tokenUri);
+    await nft.safeMint(minter.address, tokenUri);
+    await nft.connect(minter).setApprovalForAll(auction.address, true);
+    await nft.connect(owner).setApprovalForAll(auction.address, true);
+  });
+
+  describe("Listing Item", () => {
+    describe("validation", async () => {
+      it("fails if nft already has auction in play", async () => {
+        await auction
+          .connect(minter)
+          .createAuction(
+            nft.address,
+            firstTokenId,
+            "1",
+            "10000000000000",
+            true,
+            "100000000000000"
+          );
+
+        expect(
+          auction
+            .connect(minter)
+            .createAuction(
+              nft.address,
+              firstTokenId,
+              "1",
+              "10000000000000",
+              true,
+              "100000000000000"
+            )
+        ).to.be.revertedWith("auction already started");
+      });
+
+      it("fails if you don't own the nft", async () => {
+        await nft.connect(minter).safeMint(buyer.address, tokenUri);
+
+        await auction
+          .connect(minter)
+          .createAuction(
+            nft.address,
+            firstTokenId,
+            "1",
+            "10000000000000",
+            true,
+            "100000000000000"
+          );
+
+        expect(
+          auction
+            .connect(minter)
+            .createAuction(nft.address, secondTokenId, "1", "1", true, "3")
+        ).to.be.revertedWith("not owner and or contract not approved");
+      });
+    });
+
+    describe("successful creation", async () => {
+      it("Token retains in the ownership of the auction creator", async () => {
+        await nft.connect(minter).safeMint(minter.address, tokenUri);
+        await auction
+          .connect(minter)
+          .createAuction(
+            nft.address,
+            firstTokenId,
+            "1",
+            "10000000000000",
+            true,
+            "100000000000000"
+          );
+
+        const owner = await nft.ownerOf(firstTokenId);
+        expect(owner).to.be.equal(minter.address);
+      });
+    });
+  });
+
+  describe("placeBid()", async () => {
+    describe("successfully places bid", () => {
+      beforeEach(async () => {
+        await nft.connect(minter).safeMint(minter.address, firstTokenId);
+        await auction.setNowOverride("1");
+        await auction.connect(minter).createAuction(
+          nft.address,
+          firstTokenId,
+          "1", // reserve
+          "2", // start
+          true,
+          "400" // end
+        );
+      });
+
+      it("places bid and you are the top owner", async () => {
+        await auction.setNowOverride("2");
+        await auction.connect(buyer).placeBid(nft.address, firstTokenId, {
+          value: BigNumber.from(200000000000000000n),
+        });
+
+        const { _bidder, _bid } = await auction.getHighestBidder(
+          nft.address,
+          firstTokenId
+        );
+        expect(_bid).to.be.equal(BigNumber.from(200000000000000000n));
+        expect(_bidder).to.equal(buyer.address);
+        const { _reservePrice, _startTime, _endTime, _resulted } =
+          await auction.getAuction(nft.address, firstTokenId);
+        expect(_reservePrice).to.be.equal("1");
+        expect(_startTime).to.be.equal("2");
+        expect(_endTime).to.be.equal("400");
+        expect(_resulted).to.be.equal(false);
+      });
+
+      it("will refund the top bidder if found", async () => {
+        await auction.setNowOverride("2");
+        await auction
+          .connect(buyer)
+          .placeBid(nft.address, firstTokenId, { value: "200000000000000000" });
+
+        const { _bidder: originalBidder, _bid: originalBid } =
+          await auction.getHighestBidder(nft.address, firstTokenId);
+        expect(originalBid).to.be.equal(200000000000000000n);
+        expect(originalBidder).to.equal(buyer.address);
+
+        // make a new bid, out bidding the previous bidder
+        await expect(() =>
+          auction.connect(buyer2).placeBid(nft.address, firstTokenId, {
+            value: "400000000000000000",
+          })
+        ).to.changeEtherBalances(
+          [buyer, buyer2],
+          [200000000000000000n, -400000000000000000n]
+        );
+
+        const { _bidder, _bid } = await auction.getHighestBidder(
+          nft.address,
+          firstTokenId
+        );
+        expect(_bid).to.be.equal(400000000000000000n);
+        expect(_bidder).to.equal(buyer2.address);
+      });
+
+      it("successfully increases bid", async () => {
+        await auction.setNowOverride("2");
+
+        await expect(() =>
+          auction.connect(buyer).placeBid(nft.address, firstTokenId, {
+            value: "200000000000000000",
+          })
+        ).to.changeEtherBalances([buyer], [-200000000000000000n]);
+
+        const { _bidder, _bid } = await auction.getHighestBidder(
+          nft.address,
+          firstTokenId
+        );
+        expect(_bid).to.be.equal(200000000000000000n);
+        expect(_bidder).to.equal(buyer.address);
+
+        await expect(() =>
+          auction.connect(buyer).placeBid(nft.address, firstTokenId, {
+            value: "1000000000000000000",
+          })
+        ).to.changeEtherBalances(
+          [buyer],
+          [-1000000000000000000n + 200000000000000000n]
+        );
+
+        const { _bidder: newBidder, _bid: newBid } =
+          await auction.getHighestBidder(nft.address, firstTokenId);
+        expect(newBid).to.be.equal(1000000000000000000n);
+        expect(newBidder).to.equal(buyer.address);
+      });
+
+      it("successfully outbid bidder", async () => {
+        await auction.setNowOverride("2");
+
+        // Bidder 1 makes first bid
+        await expect(() =>
+          auction.connect(buyer).placeBid(nft.address, firstTokenId, {
+            value: "200000000000000000",
+          })
+        ).to.changeEtherBalances([buyer], [-200000000000000000n]);
+
+        const { _bidder, _bid } = await auction.getHighestBidder(
+          nft.address,
+          firstTokenId
+        );
+        expect(_bid).to.be.equal(200000000000000000n);
+        expect(_bidder).to.equal(buyer.address);
+
+        // Bidder 2 outbids bidder 1
+        await expect(() =>
+          auction.connect(buyer2).placeBid(nft.address, firstTokenId, {
+            value: "1000000000000000000",
+          })
+        ).to.changeEtherBalances(
+          [buyer, buyer2],
+          [200000000000000000n, -1000000000000000000n]
+        );
+
+        const { _bidder: newBidder, _bid: newBid } =
+          await auction.getHighestBidder(nft.address, firstTokenId);
+        expect(newBid).to.be.equal(1000000000000000000n);
+        expect(newBidder).to.equal(buyer2.address);
+      });
+    });
+  });
+});
