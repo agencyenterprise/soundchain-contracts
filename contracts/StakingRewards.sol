7c96a57d1fc3438398b3499175c8b9876b04f80e
Jonathan Calvo Ramirez <jono.calvo@gmail.com>
2022-03-19 17:17:28 -0700

    Staking tests for all reward phases added just 1 user

--- contracts/StakingRewards.sol
+++ contracts/StakingRewards.sol
@@ -12,11 +12,11 @@ contract StakingRewards {
     using SafeMath for uint256;
 
     address public constant REWARDS_TOKEN = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;
-    uint256 public constant OGUN_PRECISION_FACTOR = 10**12;
-    uint256 public constant REWARDS_PHASE_ONE = 307692308;
-    uint256 public constant REWARDS_PHASE_TWO = 128205128; 
-    uint256 public constant REWARDS_PHASE_THREE = 480769231; 
-    uint256 public constant REWARDS_PHASE_FOUR = 383590836; 
+    // uint256 public constant OGUN_PRECISION_FACTOR = 10**12;
+    uint256 public constant REWARDS_PHASE_ONE = 307692308000000000000;
+    uint256 public constant REWARDS_PHASE_TWO = 128205128000000000000; 
+    uint256 public constant REWARDS_PHASE_THREE = 48076923100000000000; 
+    uint256 public constant REWARDS_PHASE_FOUR = 38359083600000000000; 
     uint256 public constant PHASE_ONE_BLOCK = 195000; 
     uint256 public constant PHASE_TWO_BLOCK = 585000; 
     uint256 public constant PHASE_THREE_BLOCK = 1560000; 
@@ -26,19 +26,20 @@ contract StakingRewards {
 
     uint256 public lastUpdateBlockNumber;
     uint256 public immutable firstBlockNumber;
-    uint256 private _totalRewardsSupply = 300000000;
+    uint256 private _totalRewardsSupply;
     uint256 private _totalStaked;
     uint256 private _totalStakedTemp;
 
-    mapping(address => uint256) public rewards;
+    // mapping(address => uint256) public rewards;
 
     uint256 private _totalSupply;
     mapping(address => uint256) private _balances;
     mapping(address => bool) private _addressInserted;
     address[] private _addresses;
 
-    constructor(address _stakingToken) {
+    constructor(address _stakingToken, uint256 rawardsSupply) {
         stakingToken = IERC20(_stakingToken);
+        _totalRewardsSupply = rawardsSupply;
         firstBlockNumber = block.number;
         lastUpdateBlockNumber = block.number;
     }
@@ -49,10 +50,8 @@ contract StakingRewards {
     }
 
     function getBalanceOf(address account) external isValidAccount(account) returns (uint256) {
-        // if (!_addressInserted[account]) return 0;
         console.log('getBalanceOf Current Block number: ', block.number);
         _updateReward();
-        emit RewardsCalculated(_totalStaked);
         
         return _balances[account];
     }
@@ -65,37 +64,47 @@ contract StakingRewards {
         uint256 phase = block.number - firstBlockNumber;
         uint256 blocksToCalculate = block.number - lastUpdateBlockNumber;
         (uint256 currentRate,) = _getRewardPhaseRate(phase);
-        (uint256 previousPhaseRate, uint256 previousRateLimit) = _getRewardPhaseRate(lastUpdateBlockNumber + 1);
+        (uint256 previousPhaseRate, uint256 previousRateLimit) = _getRewardPhaseRate(lastUpdateBlockNumber - firstBlockNumber + 1);
         uint256 userBalance = _balances[user];
         uint256 previousCompound;
         uint256 previousBlocksToCalculate;
-        
+        // console.log("******block.number: ****** ", block.number);
+        // console.log("******lastUpdateBlockNumber: ****** ", lastUpdateBlockNumber);
+
         //check if last calculated phase + 1's rate is different than current's rate 
         if (currentRate != previousPhaseRate) {
-            // console.log("Not new user:", userBalance);
+        console.log("******previousPhaseRate: ****** ", previousPhaseRate);
+        console.log("******currentRate: ****** ", currentRate);
+            console.log("Switching phases:");
             previousBlocksToCalculate = previousRateLimit.sub(lastUpdateBlockNumber);
             previousCompound = _rewardPerBlock(userBalance, previousPhaseRate).mul(previousBlocksToCalculate); 
 
             blocksToCalculate = block.number.sub(previousRateLimit);
         } 
 
-        // uint256 newBalance = _rewardPerBlock(userBalance + previousCompound, currentRate).mul(blocksToCalculate);
-        uint256 newBalance = _rewardPerBlock(userBalance + previousCompound, currentRate);
-            console.log('****_rewardPerBlock****: ', newBalance);
-            newBalance = newBalance.mul(blocksToCalculate);
+        // console.log("******currentRate: ****** ", currentRate);
+        // console.log("******previousCompound: ****** ", previousCompound);
+        // console.log("******_rewardPerBlock: ****** ", _rewardPerBlock(userBalance + previousCompound, currentRate));
+        uint256 rewards = _rewardPerBlock(userBalance + previousCompound, currentRate).mul(blocksToCalculate);
+        // uint256 newBalance = _rewardPerBlock(userBalance + previousCompound, currentRate);
+        //     console.log('****_rewardPerBlock****: ', newBalance);
+        //     newBalance = newBalance.mul(blocksToCalculate);
 
-        console.log("Rate:", currentRate);
+        // console.log("Rate:", currentRate);
         console.log("blocksToCalculate:", blocksToCalculate);
-        console.log("userBalance:", userBalance);
-        console.log("New balance:", userBalance + newBalance);
-        console.log("Phase:", phase);
-        _balances[user] = _balances[user].add(newBalance);
+        // console.log("Phase:", phase);
+        uint256 newBalance = _balances[user].add(rewards);
+        console.log("Rewards:", rewards);
+        console.log("User balance:", userBalance);
+        // console.log("newBalance:", newBalance);
+        _balances[user] = newBalance;
         _totalStakedTemp += newBalance;
         
     }
 
     function _rewardPerBlock(uint256 balance, uint256 rate) private view returns (uint256) {
-        return (balance.div(_totalStaked)).mul(rate).mul(OGUN_PRECISION_FACTOR);
+        // return (balance.div(_totalStaked)).mul(rate).mul(OGUN_PRECISION_FACTOR);
+        return (balance.div(_totalStaked)).mul(rate);
     }
 
     function _getRewardPhaseRate(uint256 blockNumber) private pure returns (uint256 rate, uint256 phaseLimit) {
@@ -122,20 +131,25 @@ contract StakingRewards {
         if (block.number <= lastUpdateBlockNumber) {
             return;
         }
-        _totalStakedTemp = 0;
         for (uint256 i = 0; i < this.getAddressesSize(); i++){
             _calculateReward(_addresses[i]);
         }
         lastUpdateBlockNumber = block.number;
         _totalStaked = _totalStakedTemp;
+        _totalStakedTemp = 0;
+        // console.log('_updateReward _totalStaked: ', _totalStaked);
+
+        emit RewardsCalculated(_totalStaked);
     }
 
     function stake(uint256 _amount) external {
         require(_amount > 0, "Stake: Amount must be greater than 0");
         console.log('STAKE Current Block number: ', block.number);
+        // console.log('STAKE AMOUNT: ', _amount);
         stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
         _updateReward();
         _totalStaked += _amount;
+        // console.log('STAKE _totalStaked updated: ', _totalStaked);
         _balances[msg.sender] += _amount;
         setAddress(msg.sender);
 
@@ -145,10 +159,18 @@ contract StakingRewards {
     function withdraw() external isValidAccount(msg.sender) {
         _updateReward();
         uint256 amount = _balances[msg.sender];
-        if (amount == 0) return;
+        require(amount > 0, "Current balance is 0");
+
+        // console.log('CONTRACT withdraw Amount : ', amount);
         _balances[msg.sender] = 0;
-        if (amount > _totalStaked) amount = _totalStaked;
+        // console.log('CONTRACT withdraw _totalStaked : ', _totalStaked);
+        // if (amount > _totalRewardsSupply) {
+        //     amount = _totalRewardsSupply;
+        // }
         _totalStaked -= amount;
+        // _totalRewardsSupply -= amount;
+        // console.log('CONTRACT withdraw Amount : ', amount);
+        // console.log('Withdraw Address: ', msg.sender);
         stakingToken.safeTransfer(msg.sender, amount);
 
         emit Withdraw(msg.sender, amount);
--- test/Staking.test.ts
+++ test/Staking.test.ts
@@ -20,12 +20,6 @@ import {
 // Withdraw from user1 
 // 
 
-const advanceBlocks = async (blocks: number) =>  {
-  // for (let index = 0; index < blocks; index++) {
-    await network.provider.send('hardhat_mine', ['0x' + blocks.toString(16)]);
-  // }
-}
-
 describe("Staking", () => {
   let owner: SignerWithAddress,
     user1: SignerWithAddress, 
@@ -33,19 +27,18 @@ describe("Staking", () => {
     user3: SignerWithAddress,
     token: ERC20,
     stake: StakingRewards;
-    
-  describe("Stake contract", function () {
+
     const transfer1m = ethers.utils.parseEther("1000000");
     const transfer10k = ethers.utils.parseEther("10000");
     const transfer300m = ethers.utils.parseEther("300000000");
-
+    
     beforeEach(async () => {
       [owner, user1, user2, user3] = await ethers.getSigners();
       const tokenContract = await ethers.getContractFactory("SoundchainOGUN20");
       const StakingContract: StakingRewards__factory =
       await ethers.getContractFactory("StakingRewards");
       token = await tokenContract.deploy();
-      stake = await StakingContract.deploy(token.address);
+      stake = await StakingContract.deploy(token.address, transfer300m);
 
       await token.transfer(stake.address, transfer300m);
       await token.transfer(user1.address, transfer1m);
@@ -57,98 +50,207 @@ describe("Staking", () => {
       await token.connect(user2).approve(stake.address, transfer1m);
       await token.connect(user3).approve(stake.address, transfer1m);
     });
+  describe("Stake contract", function () {
 
-    describe("User1 stake 1 million tokens", () => {
+
+    describe("Stake calculation for different pahses with just 1 user", () => {
       it("reverts if address doesn't exist in balances", async function () {
         await expect(
           stake.callStatic.getBalanceOf(user1.address)
         ).to.be.revertedWith("address hasn't stake any tokens yet");
       });
       
-      it("should no increace balance if there is no new blocks in network", async function () {
+      it("should no increace balance with rewards if there is no new blocks in network", async function () {
         await stake.connect(user1).stake(transfer1m);
         const balance = await stake.callStatic.getBalanceOf(user1.address);
         expect(balance).to.eq(transfer1m);
       });
 
-      it("should increase balance of user 1 (by 307.6923080 phase 1)", async function () {
+      it("x1 - should increase balance of user 1 (by 307.692308 phase 1)", async function () {
         await stake.connect(user1).stake(transfer1m);
         await stake.connect(user2).stake(transfer1m);
         const balance = await stake.callStatic.getBalanceOf(user1.address);
-        expect(balance.toString()).to.eq('1000307692308000000000000');
+        expect(ethers.utils.formatEther(balance)).to.eq('1000307.692308');
       });
 
-      it("should increase balance of user 1 (by 128.205128 phase 2)", async function () {
+      it("x23 - should increase balance of user 1 (by 307.692308 x 23 phase 1)", async function () {
+        await stake.connect(user1).stake(transfer1m);
+        await network.provider.send('hardhat_mine', ['0x17']);
+        const balance = await stake.callStatic.getBalanceOf(user1.address);
+        expect(ethers.utils.formatEther(balance)).to.eq('1007076.923084');
+      });
+
+      it("x1 - should increase balance of user 1 (by 128.205128 phase 2)", async function () {
         await network.provider.send('hardhat_mine', ['0x30000']);
         await stake.connect(user1).stake(transfer1m);
         await stake.connect(user2).stake(transfer1m);
         const balance = await stake.callStatic.getBalanceOf(user1.address);
-        expect(balance.toString()).to.eq('1000128205128000000000000');
+        expect(ethers.utils.formatEther(balance)).to.eq('1000128.205128');
       });
 
 
-      it("should increase balance of user 1 (by 48.0769231 phase 3)", async function () {
-        // await network.provider.send('hardhat_mine', ['0x180000']);
+      it("x343 - should increase balance of user 1 (by 128.205128 x 343 phase 2)", async function () {
+        await network.provider.send('hardhat_mine', ['0x30000']);
+        await stake.connect(user1).stake(transfer1m);
+        await network.provider.send('hardhat_mine', ['0x157']);
+        const balance = await stake.callStatic.getBalanceOf(user1.address);
+        expect(ethers.utils.formatEther(balance)).to.eq('1043974.358904');
+      });
+
+      it("x1 - should increase balance of user 1 (by 48.0769231 phase 3)", async function () {
         await network.provider.send('hardhat_mine', ['0x90000']);
         await stake.connect(user1).stake(transfer1m);
         await stake.connect(user2).stake(transfer1m);
         const balance = await stake.callStatic.getBalanceOf(user1.address);
-        expect(balance.toString()).to.eq('10000480769231000000000000');
+        expect(ethers.utils.formatEther(balance)).to.eq('1000048.0769231');
       });
+      
+      it("x71 - should increase balance of user 1 (by 48.0769231 x 71 phase 3)", async function () {
+        await network.provider.send('hardhat_mine', ['0x90000']);
+        await stake.connect(user1).stake(transfer1m);
+        await network.provider.send('hardhat_mine', ['0x47']);
+        const balance = await stake.callStatic.getBalanceOf(user1.address);
+        expect(ethers.utils.formatEther(balance)).to.eq('1003413.4615401');
+      });
+
+      it("x1 - should increase balance of user 1 (by 38.3590836 phase 4)", async function () {
+        await network.provider.send('hardhat_mine', ['0x180000']);
+        await stake.connect(user1).stake(transfer1m);
+        await stake.connect(user2).stake(transfer1m);
+        const balance = await stake.callStatic.getBalanceOf(user1.address);
+        expect(ethers.utils.formatEther(balance)).to.eq('1000038.3590836');
+      });
+
+      it("x51 - should increase balance of user 1 (by 38.3590836 x 51 phase 4)", async function () {
+        await network.provider.send('hardhat_mine', ['0x180000']);
+        await stake.connect(user1).stake(transfer1m);
+        await network.provider.send('hardhat_mine', ['0x33']);
+        const balance = await stake.callStatic.getBalanceOf(user1.address);
+        expect(ethers.utils.formatEther(balance)).to.eq('1001956.3132636');
+      });
+
+    describe("Withdraw", () => {
+      it("should transfer current balance to user", async function () {
+        await stake.connect(user1).stake(transfer1m);
+        await stake.connect(user1).withdraw();
+        const user1Balance = await token.balanceOf(user1.address);
+        expect(ethers.utils.formatEther(user1Balance)).to.eq('1000307.692308');
+      });
+
+      // it("should substract transfer current balance to user", async function () {
+      //   await stake.connect(user1).stake(transfer1m);
+      //   await stake.connect(user1).withdraw();
+
+      //   const user1Balance = await token.balanceOf(user1.address);
+      //   const tokenBalance = await token.balanceOf(stake.address);
+      //   console.log('New TOKEN BALANCE USER string: ', user1Balance.toString());
+      //   console.log('New TOKEN BALANCE USER 1: ', ethers.utils.formatEther(user1Balance));
+      //   console.log('New TOKEN BALANCE Stake contrect: ', ethers.utils.formatEther(tokenBalance));
+      //   expect(ethers.utils.formatEther(user1Balance)).to.eq('1000307.692308');
+      // });
+    });
+
+
         // await stake.connect(user2).stake(transfer1m);
-        // await stake.connect(user3).stake(transfer1m);
-              //await (await stake.getBalanceOf(user1.address)).toString()
-        // const result = await stake.getBalanceOf(user1.address);
-        // console.log('CUERRENT BALANCE user1: ', result);
+        // const balance = await stake.callStatic.getBalanceOf(user1.address);
+        // const rc = await tx.wait();
+        // console.log('Withdrwa TX: ', tx);
+
+
+        // await stake.connect(user1).stake(transfer1m);
+        // await stake.connect(user2).stake(transfer1m);
+
+      //   // await network.provider.send('hardhat_mine', ['0x180000']);
+        // await network.provider.send('hardhat_mine', ['0x1']);
+        // const addres2Bal = await token.balanceOf(user1.address);
+        // console.log('addres2Bal: ', addres2Bal.toString());
+      //   // const stakingContractBalance = await token.balanceOf(stake.address);
+      //   // console.log('New Staking Contract bal: ', stakingContractBalance.toString());
+      //   // await stake.connect(user1).withdraw();
+      //   // expect(balance.toString()).to.eq('10000480769231000000000000');
 
         // await network.provider.send("evm_mine");
         // await ethers.provider.send('hardhat_mine', ['0x100']);
-        // advanceBlocks(256);
-        // await stake.addBlock();
-        // await stake.addBlock();
-        
-        // //approve user2 and stake 1000000
-        // await token.connect(user2).approve(stake.address, transfer1m);
-        // await stake.connect(user2).stake(transfer1m);
-
 
         // console.log('balance  after stakes of contract stake: ', (await token.balanceOf(stake.address)).toString());
         // console.log('balances in stakeof user 1: ', await (await stake.getBalanceOf(user1.address)).toString());
 
-
-
-
-
-
-        // await token.connect(user3).approve(stake.address, transfer1m);
-        // await stake.connect(user3).stake(transfer1m);
-        // console.log('balances in stakeof user 1: ', await (await stake.getBalanceOf(user1.address)).toString());
-        // console.log('balances in stakeof user 2: ', await (await stake.getBalanceOf(user2.address)).toString());
-
-        
-        // console.log('balance  token user 1: ', (await token.balanceOf(user1.address)).toString());
-        // console.log('STAKE CONTRACT: ', stake);
-        
         // const supplyreward = await stake.totalStakeSupply();
         // console.log('reward supply: ', supplyreward.toString());
 
-        // const supplyreward = await stake.totalStakeSupply;
-        // console.log('reward supply: ', supplyreward);
-        // await stake.connect(user2).stake(transferAmount);
-        // const reward = await stake.connect(user1).withdraw;
-        // console.log('reward: ', reward);
-
-        // setTimeout(() => {console.log("wait 5 sec")}, 5000);
-
-        
-
-        // const expectedBalance = utils.parseEther("999000000");
-        // const balance = await token.balanceOf(owner.address);
-        // expect(balance).to.eq(expectedBalance);
-
     });
+
+
+
   });
 });
+
+// describe("Staking 2", () => {
+//   let owner: SignerWithAddress,
+//     user1: SignerWithAddress, 
+//     user2: SignerWithAddress,
+//     user3: SignerWithAddress,
+//     token: ERC20,
+//     stake: StakingRewards;
+
+
+//   // describe("Stake contract", function () {
+//   //   const transfer1m = ethers.utils.parseEther("1000000");
+//   //   const transfer10k = ethers.utils.parseEther("10000");
+//   //   const transfer300m = ethers.utils.parseEther("300000000");
+
+//   //   beforeEach(async () => {
+//   //     [owner, user1, user2, user3] = await ethers.getSigners();
+//   //     const tokenContract = await ethers.getContractFactory("SoundchainOGUN20");
+//   //     const StakingContract: StakingRewards__factory =
+//   //     await ethers.getContractFactory("StakingRewards");
+//   //     token = await tokenContract.deploy();
+//   //     stake = await StakingContract.deploy(token.address);
+
+//   //     await token.transfer(stake.address, transfer300m);
+//   //     await token.transfer(user1.address, transfer1m);
+//   //     await token.transfer(user2.address, transfer1m);
+//   //     await token.transfer(user3.address, transfer1m);
+
+
+//   //     await token.connect(user1).approve(stake.address, transfer1m);
+//   //     await token.connect(user2).approve(stake.address, transfer1m);
+//   //     await token.connect(user3).approve(stake.address, transfer1m);
+//   //   });
+
+
+//   //   // describe("Withdraw", () => {
+//   //   //   // it("Balance shoudl be higher of 1m", async function () {
+
+//   //   //   //   await stake.connect(user1).stake(transfer1m);
+//   //   //   //   await network.provider.send("evm_mine");
+//   //   //   //   const stakeBalance = await stake.callStatic.getBalanceOf(user1.address);
+//   //   //   //   // await stake.connect(user1).withdraw();
+//   //   //   //   // await stake.connect(user2).stake(transfer1m);
+//   //   //   //   // await network.provider.send('hardhat_mine', ['0x1']);
+//   //   //   //   // await stake.connect(user1).stake(transfer1m);
+//   //   //   //   // const addres2Bal = await token.balanceOf(user1.address);
+//   //   //   //   // console.log('addres1Bal: ', stakeBalance.toString());
+//   //   //   //   // await stake.connect(user2).stake(transfer1m);
+//   //   //   // });
+
+
+//   //   //   it("**** s in network ******", async function () {
+//   //   //     await stake.connect(user1).stake(transfer1m);
+//   //   //     await stake.connect(user2).stake(transfer1m);
+//   //   //     // await network.provider.send("evm_mine");
+//   //   //     const balance = await stake.callStatic.getBalanceOf(user1.address);
+//   //   //     expect(balance).to.eq(transfer1m);
+//   //   //   });
+
+//   //   // });
+
+
+    
+
+//   // });
+
+// });
 
 
         //approve user1 and stake 1000000
